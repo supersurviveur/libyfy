37ba86a618afb1c3cbe9bf0a26b82c28f94846b6
supersurviveur <julien.thillard38@gmail.com>
2024-06-12 20:32:12 +0200

    clean main file

--- main.c
+++ main.c
@@ -3,13 +3,6 @@
 #include "libyfy.h"
 
 int main() {
-    // int *vector = vector_create(int);
-    // vector_push(vector, 23000);
-    // vector_push(vector, 23010);
-    // vector_push(vector, 25);
-    // vector_push(vector, 34);
-    // print_int_vector(vector, false);
-    // vector_free(vector);
 
     TIME_START(1);
     TRACE("Hello from main");
@@ -20,7 +13,7 @@ int main() {
     TIME_END(1);
     TIME_END_MS(1);
     TIME_END_S(1);
-    // PANIC("This is a panic message"
+    // PANIC("This is a panic message");
 
     
     return 0;
